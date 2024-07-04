2fc840db5fd9d38570a7488ed9a67fdc0de3418b
Oscar Rovira <cruzrovira@gmail.com>
2024-07-04 13:51:26 -0600

    add keymaps control + up , control + down

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.api.nvim_set_keymap('n', '<M-Up>', ':m-2<CR>', { noremap = true, silent = true });
-vim.api.nvim_set_keymap('n', '<M-Down>', ':m+1<CR>', { noremap = true, silent = true });
+vim.api.nvim_set_keymap('n', '<C-Up>', ':m-2<CR>', { noremap = true, silent = true });
+vim.api.nvim_set_keymap('n', '<C-Down>', ':m+1<CR>', { noremap = true, silent = true });
 
 ----- Tmux Navigation ------
 local nvim_tmux_nav = require("nvim-tmux-navigation")
