4893cdc8369e2bd00887ec0d237f9e520b5215e0
ozcar <cruzrovira@gmail.com>
2025-06-10 22:19:28 -0600

    Update lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,6 +14,21 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank", -- Use win32yank for clipboard operations
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf", -- Command to copy to the system clipboard
+      ["*"] = "win32yank.exe -i --crlf", -- Command to copy to the primary clipboard
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf", -- Command to paste from the system clipboard
+      ["*"] = "win32yank.exe -o --lf", -- Command to paste from the primary clipboard
+    },
+    cache_enabled = false, -- Disable clipboard caching
+  }
+end
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
